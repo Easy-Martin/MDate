01442771ced06a09077e908f1ab7f396b8a136b9
huangjingjing <253887111@qq.com>
2016-12-01 14:59:37 +0800

    Update MDate.js

--- MDate.js
+++ MDate.js
@@ -11,8 +11,9 @@ class MDate {
       'D+': this._D.getDate(),
       'h+': this._D.getHours(),
       'm+': this._D.getMinutes(),
-      's+': this._D.getSeconds().toString(),
-      'q+': Math.floor((this._D.getMonth() + 3) / 3),
+      's+': this._D.getSeconds(),
+      'q': Math.floor((this._D.getMonth() + 3) / 3),
+      'S': this._D.getMilliseconds()
     }
     if (new RegExp(/(Y+)/).test(F)) F = F.replace(RegExp.$1, this._D.getFullYear().toString()).substr(4 - RegExp.$1.length);
     for (let K in O) {
@@ -20,8 +21,9 @@ class MDate {
         F = F.replace(RegExp.$1, (RegExp.$1.length == 1) ? (O[K].toString()) : (O[K] < 10) ? ('0' + O[K].toString()) : (O[K].toString()))
       }
     }
-    if (new RegExp(/(S)/).test(F)) F = F.replace(RegExp.$1, this._D.getMilliseconds().toString());
+
     return (F ? F : this._D.toUTCString())
   }
 }
 module.exports = MDate
+
