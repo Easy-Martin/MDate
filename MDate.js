e5c44383f3aa4e98106aaeaa671b4c4485f2b003
huangjingjing <huangjingjing@wondersgroup.com>
2017-01-12 17:24:05 +0800

    demo.html

--- MDate.js
+++ MDate.js
@@ -1,28 +1,37 @@
 /**
  * Created by wonders on 2016/12/1.
  */
-;(function(global){
-    function MDate (D){
-        this._D = D ? new Date(D.replace(/\-/g, '/')) : new Date();
-    }
-    MDate.prototype.Format = function (F) {
-        var O = {
-        'M+': this._D.getMonth() + 1,
-        'D+': this._D.getDate(),
-        'h+': this._D.getHours(),
-        'm+': this._D.getMinutes(),
-        's+': this._D.getSeconds(),
-        'q': Math.floor((this._D.getMonth() + 3) / 3),
-        'S': this._D.getMilliseconds()
+;
+(function(global) {
+    function MDate(D, F) {
+        if (typeof D !== 'string' || typeof F !== 'string') {
+            throw new Error('param is a string')
         }
-        if (new RegExp(/(Y+)/).test(F)) F = F.replace(RegExp.$1, this._D.getFullYear().toString()).substr(4 - RegExp.$1.length);
+        this._D = new Date(D.replace(/\-/g, '/'));
+        let res = DateFormat(this._D, F);
+        this._D = null;
+        return res;
+    }
+
+    function DateFormat(D, F) {
+        var O = {
+            'M+': D.getMonth() + 1,
+            'D+': D.getDate(),
+            'h+': D.getHours(),
+            'm+': D.getMinutes(),
+            's+': D.getSeconds(),
+            'q': Math.floor((D.getMonth() + 3) / 3),
+            'S': D.getMilliseconds()
+        }
+        if (new RegExp(/(Y+)/).test(F)) F = F.replace(RegExp.$1, D.getFullYear().toString()).substr(4 - RegExp.$1.length);
         for (var K in O) {
             if (new RegExp('(' + K + ')').test(F)) {
                 F = F.replace(RegExp.$1, (RegExp.$1.length == 1) ? (O[K].toString()) : (O[K] < 10) ? ('0' + O[K].toString()) : (O[K].toString()))
             }
         }
-
-        return (F ? F : this._D.toUTCString())
+        var result = D.toUTCString();
+        D = null;
+        return (F ? F : result)
     }
 
     if (typeof module !== 'undefined' && module.exports) {
@@ -35,6 +44,3 @@
         global.MDate = MDate;
     };
 })(this)
-
-
-
--- README.md
+++ README.md
@@ -5,20 +5,17 @@ npm i mini-date-format --save
 
 import MDate from 'mini-date-format'
 
-new MDate().Format('YYYY-MM-DD hh:mm:ss q S');//  2016-12-01 14:20:01 4 607
-new MDate().Format('YYYY-MM-DD');//  2016-12-01
+MDate(M,F);
 
-
-new MDate('2015-11-01 15:25:58').Format('YYYY-MM-DD hh:mm:ss q S');//  2015-11-01 15:25:58 4 0
-new MDate('2015-11-01 15:25:58').Format('YYYY-MM-DD');//  2015-11-01
+MDate('2015-11-01 15:25:58','YYYY-MM-DD')//  2015-11-01
 
 ```
-##Format
+
 
 ```
 @param
-
-
+M-->'2016-12-01 14:20:01'
+F-->'YYYY-MM-DD mm:ss:qq S'
 'Y' ----- 'Year'
 'M' ----- 'Month'
 'D' ----- 'Day'
